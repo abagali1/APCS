403176691d6cc3daf21fab14575e86166e562af8
abagali1 <a.bagali815703@gmail.com>
2019-01-07 11:19:58 -0500

    (

--- 06 Stacks_Queues/01 ParenMatch/ParenMatch.java
+++ 06 Stacks_Queues/01 ParenMatch/ParenMatch.java
@@ -1,18 +1,29 @@
-// Name:
-// Date:
+// Name: Anup Bagali
+// Date: 1/7/19
 
 import java.util.*;
 
 public class ParenMatch
 {
-   public static final String left  = "([{<";
-   public static final String right = ")]}>";
+   public static final String LEFT  = "([{<";
+   public static final String RIGHT = ")]}>";
    
    public static void main(String[] args)
    {
       System.out.println("Parentheses Match");
       ArrayList<String> parenExp = new ArrayList<String>();
-      /* enter test cases here */
+      parenExp.add("5+7");
+      parenExp.add("(5+7)");
+      parenExp.add(")5+7(");
+      parenExp.add("((5+7)*3)");
+      parenExp.add("<{5+7}*3>");
+      parenExp.add("[(5+7)*]3");
+      parenExp.add("(5+7)*3");
+      parenExp.add("5+(7*3)");
+      parenExp.add("((5+7)*3");
+      parenExp.add("[(5+7]*3)");
+      parenExp.add("[(5+7)*3])");
+      parenExp.add("([(5+7)*3]");
       
    
       for( String s : parenExp )
@@ -27,6 +38,31 @@ public class ParenMatch
    
    public static boolean checkParen(String exp)
    {
+      char[] arr = exp.toCharArray();
+      Stack<Character> stack = new Stack<Character>();
+      boolean result = false;
+      for(char c: arr){
+         if(LEFT.contains(""+c)){
+            stack.push(c);
+         }
+         else if(RIGHT.contains(""+c)){
+            try{
+               Character temp = stack.pop();
+               if(LEFT.indexOf(c) == RIGHT.indexOf(temp)){
+                  result = true;
+               }
+            }
+            catch(EmptyStackException e){
+               result = false;
+            }
+         }
+      }
+      if(!stack.isEmpty()){
+         result = false;
+      }else{
+         result = true;
+   }
+      return result;
    
    }
 }
