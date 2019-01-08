01a8ab0f2e776c6f5ed0e2e3fa34bc7209031249
abagali1 <a.bagali815703@gmail.com>
2019-01-08 13:19:44 -0500

    postfix

--- 06 Stacks_Queues/02 Postfix/Postfix.java
+++ 06 Stacks_Queues/02 Postfix/Postfix.java
@@ -1,16 +1,28 @@
-// Name:
-// Date:
+// Name: Anup Bagali 
+// Date: 1/8/19
 
 import java.util.*;
 
 public class Postfix
-{
+{  
+   public static final String OPERATORS = "+-/*^%!";
    public static void main(String[] args)
    {
       System.out.println("Postfix  -->  Evaluate");
       ArrayList<String> postfixExp = new ArrayList<String>();
-      /*  build your list of expressions here  */
-   
+      postfixExp.add("3 4 5 * +");
+      postfixExp.add("3 4 * 5 +");
+      postfixExp.add("10 20 + -6 6 * +");
+      postfixExp.add("3 4 + 5 6 + *");
+      postfixExp.add("3 4 5 + * 2 - 5 /");
+      postfixExp.add("8 1 2 * + 9 3 / -");
+      postfixExp.add("2 3 ^");
+      postfixExp.add("20 3 %");
+      postfixExp.add("21 3 %");
+      postfixExp.add("22 3 %");
+      postfixExp.add("23 3 %");
+      postfixExp.add("5 !");
+      postfixExp.add("1 1 1 1 1 + + + + !");
       
    
       
@@ -23,18 +35,62 @@ public class Postfix
    public static int eval(String pf)
    {
       List<String> postfixParts = new ArrayList<String>(Arrays.asList(pf.split(" ")));
-      /*  enter your code here  */
+      Stack<Integer> stack = new Stack<Integer>();
+      for(String s: postfixParts){
+         if(!isOperator(s)){
+            stack.add(new Integer(s));
+         }
+         else{
+            if(!"!".equals(s)){
+               int second = (int)stack.pop();
+               int first = (int)stack.pop();
+               stack.push(new Integer(eval(first,second,s)));
+            }
+            else{
+               int first = (int)stack.pop();
+               stack.push(new Integer(eval(first,0,s)));
+            }
+         }
+      }
+      return (int)stack.pop();
    
    }
    
    public static int eval(int a, int b, String ch)
    {
-   
+      if("!".equals(ch)){
+         return factorial(a);
+      }
+      else if("+".equals(ch)){
+         return a+b;
+      }
+      else if("-".equals(ch)){
+         return a-b;
+      }
+      else if("*".equals(ch)){
+         return a*b;
+      }
+      else if("^".equals(ch)){
+         return (int)Math.pow(a,b);
+      }
+      else if("%".equals(ch)){
+         return a%b;
+      }
+      else{
+         return a/b;
+      }
    }
    
    public static boolean isOperator(String op)
    {
-   
+      return OPERATORS.contains(op);
+   }
+   public static int factorial(int num){
+      if(num == 1 || num == 0){
+         return 1;
+      }else{
+         return num * factorial(num-1);
+      }
    }
 }
 
