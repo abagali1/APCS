6dac715170402bfa955b20f933728c8862e0222f
abagali1 <a.bagali815703@gmail.com>
2019-04-26 00:35:32 -0400

    Floyd

--- 12 Graphs/00-02 AdjMat/AdjMat.java
+++ 12 Graphs/00-02 AdjMat/AdjMat.java
@@ -38,7 +38,7 @@ interface Floyd
    void allPairsWeighted(); 
 }
 
-public class AdjMat implements AdjacencyMatrix, Warshall 
+public class AdjMat implements AdjacencyMatrix, Warshall, Floyd
 {
    private int[][] grid = null;   //adjacency matrix representation
    private Map<String, Integer> vertices = null;   // name-->index (for Warshall & Floyd)
@@ -49,6 +49,70 @@ public class AdjMat implements AdjacencyMatrix, Warshall
    } 
    public Map<String,Integer> getVertices(){
       return vertices;
+   }
+   public void addEdge(int source, int target){
+      grid[source][target] = 1;
+   }
+   public void removeEdge(int source,int target){
+      grid[source][target] = 0;
+   }
+   public boolean isEdge(int source,int target){
+      if(source >= grid.length || target >= grid[0].length-1){
+         return false;
+      } 
+      return grid[source][target] > 0;
+   }
+   public boolean isEdge(String source, String target){
+      return grid[vertices.get(source)][vertices.get(target)] > 0;
+   }
+   public int edgeCount(){
+      int result = 0;
+      for(int i=0;i<=grid.length-1;i++){
+         for(int j=0;j<=grid[0].length-1;j++){
+            if(grid[i][j] > 0 && grid[i][j] != 9999)
+               result++;
+         }
+      }
+      return result;
+   }
+   public List<Integer> getNeighbors(int source){
+      if(source >= grid.length){
+         return null;
+      }
+      List<Integer> result = new ArrayList<Integer>();
+      for(int i=0;i<=grid[source].length-1;i++){
+         if(grid[source][i] > 0)
+            result.add(i);
+      }
+      return result;
+   }
+   public int getCost(int from, int to){
+      return grid[from][to];
+   }
+   public int getCost(String from, String to){
+      return grid[vertices.get(from)][vertices.get(to)];
+   }
+   public void allPairsReachability(){
+      for(String s: vertices.keySet()){
+         for(int i=0;i<=grid.length-1;i++){
+            for(int j=0;j<=grid.length-1;j++){
+               if(grid[i][vertices.get(s)]==1 && grid[vertices.get(s)][j]==1 ){
+                  grid[i][j] = 1;
+               }
+            }
+         }
+      }
+   }
+   public void allPairsWeighted(){
+      for(String s: vertices.keySet()){
+         for(int i=0;i<=grid.length-1;i++){
+            for(int j=0;j<=grid.length-1;j++){
+               if(grid[i][vertices.get(s)] + grid[vertices.get(s)][j] < grid[i][j] ){
+                  grid[i][j] = grid[i][vertices.get(s)] + grid[vertices.get(s)][j];
+               }
+            }
+         }
+      }
    }
    public void readGrid(String fileName) throws FileNotFoundException{
       Scanner infile = new Scanner(new File(fileName));
@@ -78,53 +142,6 @@ public class AdjMat implements AdjacencyMatrix, Warshall
       for(String s: vertices.keySet()){
          System.out.println(vertices.get(s)+"-"+s);
       }
-   }
-   public void allPairsReachability(){
-      for(String s: vertices.keySet()){
-         for(int i=0;i<=grid.length-1;i++){
-            for(int j=0;j<=grid.length-1;j++){
-               if(grid[i][vertices.get(s)]==1 && grid[vertices.get(s)][j]==1 ){
-                  grid[i][j] = 1;
-               }
-            }
-         }
-      }
-   }
-   public void addEdge(int source, int target){
-      grid[source][target] = 1;
-   }
-   public void removeEdge(int source,int target){
-      grid[source][target] = 0;
-   }
-   public boolean isEdge(int source,int target){
-      if(source >= grid.length || target >= grid[0].length-1){
-         return false;
-      } 
-      return grid[source][target] > 0;
-   }
-   public boolean isEdge(String source, String target){
-      return grid[vertices.get(source)][vertices.get(target)] > 0;
-   }
-   public int edgeCount(){
-      int result = 0;
-      for(int i=0;i<=grid.length-1;i++){
-         for(int j=0;j<=grid[0].length-1;j++){
-            if(grid[i][j] > 0)
-               result++;
-         }
-      }
-      return result;
-   }
-   public List<Integer> getNeighbors(int source){
-      if(source >= grid.length){
-         return null;
-      }
-      List<Integer> result = new ArrayList<Integer>();
-      for(int i=0;i<=grid[source].length-1;i++){
-         if(grid[source][i] > 0)
-            result.add(i);
-      }
-      return result;
    }
    public String toString(){
       String result = "";
